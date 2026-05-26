3fbc4a991793a77bf544eba7acd55b7ed6582c36
Cole Sullivan <cole.sullivan@hca.nm.gov>
2026-05-26 11:37:51 -0600

    Update app.js

--- dashboard/app.js
+++ dashboard/app.js
@@ -3,12 +3,11 @@ const monday = window.mondaySdk();
 async function init() {
   try {
     const context = await monday.get('context');
-    currentItemId = context?.data?.itemId;
     currentBoardId = context?.data?.boardId;
 
     console.log(context.data);
 
-    if (!currentItemId || !currentBoardId) {
+    if (!currentBoardId) {
       throw new Error('Please open this in a monday object.');
     }
 
