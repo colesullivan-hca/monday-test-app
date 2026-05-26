9165c1c1536abec3f0a1fe9bcbe943b94b229719
Cole Sullivan <cole.sullivan@hca.nm.gov>
2026-05-26 11:35:49 -0600

    test api

--- dashboard/app.js
+++ dashboard/app.js
@@ -12,9 +12,19 @@ async function init() {
       throw new Error('Please open this in a monday object.');
     }
 
+    const query = `query {
+        boards(ids: [18412077420]) {
+          columns(types: [status]) {
+            id
+            settings
+          }
+        }
+      }`;
+      const res = await monday.api(query);
+      console.log(res.data);
+
   } catch (err) {
     console.error('Init error:', err);
   }
 }
-
 init();
