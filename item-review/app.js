a154fbd6008b3d086a346783b0c9b3a1fb9b47db
Cole Sullivan <cole.sullivan@hca.nm.gov>
2026-06-02 08:49:48 -0600

    changes

--- item-review/app.js
+++ item-review/app.js
@@ -901,10 +901,7 @@ function docCard(asset, item, filesColId) {
       <div class="doc-card-icon">${emoji}</div>
       <div class="doc-card-name">${escHtml(asset.name)}</div>
       <div class="doc-card-meta">${ext}${size ? ' · ' + size : ''}${date ? ' · ' + date : ''}</div>
-      <a class="doc-card-open" href="${escHtml(asset.public_url)}" target="_blank" rel="noopener">
-        Open
-        <svg viewBox="0 0 20 20" fill="currentColor"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"/><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"/></svg>
-      </a>
+      <button class="doc-card-open" onclick="openMondayFileViewer('${escHtml(asset.id)}', '${escHtml(item.id)}', '${escHtml(filesColId)}')">
     </div>
   `;
 }
